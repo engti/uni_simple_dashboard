1737269d2dbdccd5d45aab158e52c530047f13f0
engti <rbrowne@gmail.com>
2018-02-04 14:23:47 +0530

    first commit, with auth

---
+++ main.R
@@ -0,0 +1,12 @@
+## file to test analytics calls
+
+# load library ----
+  library(dplyr)
+  library(RSiteCatalyst)
+  library(leaflet)
+  library(plotly)
+
+# site catalyst authentication
+  source("auth.R")
+
+# 
